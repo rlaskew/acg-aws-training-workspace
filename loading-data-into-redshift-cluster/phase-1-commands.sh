6747dd989615716f6cff1c13605a824a2dca650c
rlaskew <richardaskew@yahoo.com>
2021-11-02 12:21:50 -0500

    Update phase-1-commands.sh

--- loading-data-into-redshift-cluster/phase-1-commands.sh
+++ loading-data-into-redshift-cluster/phase-1-commands.sh
@@ -7,3 +7,6 @@ aws s3 mb s3://${BUCKET_NAME}
 aws s3api put-object --bucket ${BUCKET_NAME} --key redshift-data.csv --body redshift-data.csv
 curl -O -l https://raw.githubusercontent.com/linuxacademy/content-aws-database-specialty/master/S06_Additional%20Database%20Services/redshift-data.json
 aws dynamodb create-table --table-name redshift-import --attribute-definitions AttributeName=ID,AttributeType=N --key-schema AttributeName=ID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+aws dynamodb list-tables
+aws dynamodb batch-write-item --request-items file://redshift-data.json
+aws dynamodb scan --table-name redshift-import
